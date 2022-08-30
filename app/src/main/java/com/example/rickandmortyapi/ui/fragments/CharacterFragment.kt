3297e8b5e45236e5c826d29174889706196cb6bb
Packetic <bariy.litievich@gmail.com>
2022-08-30 19:36:21 +0300

    retrofit works, also made nice card design

--- app/src/main/java/com/example/rickandmortyapi/ui/fragments/CharacterFragment.kt
+++ app/src/main/java/com/example/rickandmortyapi/ui/fragments/CharacterFragment.kt
@@ -6,24 +6,66 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.navArgs
+import com.bumptech.glide.Glide
+import com.example.rickandmortyapi.R
 import com.example.rickandmortyapi.databinding.FragmentCharacterBinding
 import com.example.rickandmortyapi.ui.stateholder.CharacterViewModel
 
 class CharacterFragment : Fragment() {
+    lateinit var viewModel: CharacterViewModel
     private var _binding: FragmentCharacterBinding? = null
     private val binding get() = _binding!!
+    private val args: CharacterFragmentArgs by navArgs()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val viewModel = ViewModelProvider(this)[CharacterViewModel::class.java]
         _binding = FragmentCharacterBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel = ViewModelProvider(this)[CharacterViewModel::class.java]
+        viewModel.loadCharacterData(args.id)
+        observeViewModel()
     }
 
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    private fun observeViewModel() {
+        viewModel.character.observe(viewLifecycleOwner) {
+            if (it != null) {
+                binding.charGender.text = it.body()?.gender
+                binding.charLocation.text = it.body()?.location?.name
+                binding.charName.text = it.body()?.name
+                binding.charOrigin.text = it.body()?.origin?.name
+                binding.charSpecies.text = it.body()?.species
+                binding.charStatus.text = it.body()?.status
+                loadImage(it.body()?.image)
+                stateSelector()
+            }
+        }
+    }
+
+    private fun loadImage(image: String?) {
+        Glide.with(requireContext())
+            .load(image)
+            .fitCenter()
+            .into(binding.charImage)
+    }
+
+    private fun stateSelector() {
+        when(binding.charStatus.text) {
+            "Alive" -> binding.charImage.setStrokeColorResource(R.color.green)
+            "Dead" -> binding.charImage.setStrokeColorResource(R.color.red)
+            "Unknown" -> binding.charImage.setStrokeColorResource(R.color.gray)
+            else -> binding.charImage.setStrokeColorResource(R.color.white)
+        }
     }
 }
--- app/src/main/java/com/example/rickandmortyapi/ui/fragments/SearchFragment.kt
+++ app/src/main/java/com/example/rickandmortyapi/ui/fragments/SearchFragment.kt
@@ -1,11 +1,13 @@
 package com.example.rickandmortyapi.ui.fragments
 
 import android.os.Bundle
+import android.text.Editable
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.navigation.fragment.findNavController
 import com.example.rickandmortyapi.databinding.FragmentSearchBinding
 
 class SearchFragment : Fragment() {
@@ -23,15 +25,11 @@ class SearchFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding.btnFind.setOnClickListener {
-            // TODO: переписать нормально, это кринж
-            if (binding.numId.text != null) {
-                val text = binding.numId.text.toString().toInt()
-                if (text in 1..20) {
-                    // TODO: go to CharacterFragment with text as argument
-                }
-                else {
-                    Toast.makeText(context, "Input correct number", Toast.LENGTH_LONG).show()
-                }
+            // TODO: let me introduce my cringe-driven development
+            if (binding.numId.text != null && binding.numId.text.toString().toIntOrNull() in 1..200) {
+                val num = binding.numId.text.toString().toInt()
+                val action = SearchFragmentDirections.actionSearchFragmentToCharacterFragment(num)
+                findNavController().navigate(action)
             }
             else {
                 Toast.makeText(context, "Input correct number", Toast.LENGTH_LONG).show()
